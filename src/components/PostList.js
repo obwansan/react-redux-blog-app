e982c0a67bbcd218bb5ae767187beb3cbf875a21
Kevin O'Brien <kevin.obrien@lovehoney.co.uk>
2019-02-01 16:26:02 +0000

    Fetch data in a redux app and wire up an action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostList extends React.Component {
+  
+  // When PostList is rendered, componentDidMount is called, which 
+  // calls the fetchposts action creator.
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+export default connect(null, {fetchPosts: fetchPosts})(PostList);
